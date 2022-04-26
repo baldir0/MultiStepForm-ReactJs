cf52ca60f7070088268ecdd4451fd2328e77df2f
baldir0 <lewucha2000@gmail.com>
2022-04-26 22:52:50 +0200

    CSS fix

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import styles from './../../App.css';
+import './Field.module.css';
 
 export default function Field({ attributes, register, events }) {
   const name = attributes.name;
   const type = attributes.type;
   const isButton = type === 'button' || type === 'submit';
   return isButton ? (
-    <input className={styles.button} {...attributes} {...events} />
+    <input className={"button"} {...attributes} {...events} />
   ) : (
-    <input className={styles.input} {...attributes} {...register(name)} {...events} />
+    <input className={"button"} {...attributes} {...register(name)} {...events} />
   );
 }
